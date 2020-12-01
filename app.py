aa2a023bc734fc5a352d1763391eadc30c59944e
Aetrix27 <davedogg323@gmail.com>
2020-11-30 22:53:34 -0800

    Updated documents page and feature page with location

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import jinja2
+import os
 
 from pprint import PrettyPrinter
 from flask import Flask, request, render_template
@@ -8,6 +9,8 @@ from flask import Flask, request, render_template
 ################################################################################
 
 app = Flask(__name__)
+
+API_KEY = os.getenv('API_KEY')
 
 my_loader = jinja2.ChoiceLoader([
     app.jinja_loader,
@@ -25,20 +28,25 @@ pp = PrettyPrinter(indent=4)
 def home():
     """Displays the homepage with forms for current or historical data."""
 
-    return render_template('index.html')
+    return render_template('index.html') 
 
 
 @app.route('/uploaddocs')
 def uploaddocs():
     """Displays the homepage with forms for current or historical data."""
+    
+    sampleDataTitle = ["Medical Document 1", "Medical Document 2", "Medical Document 3",
+                        "Medical Document 4", "Medical Document 5", "Medical Document 6"]
 
     sampleData = ["This is Medical Document 1", "This is Medical Document 2", 
                 "This is Medical Document 3", "This is Medical Document 4", 
                 "This is Medical Document 5", "This is Medical Document 6"]
+
     counter = 0 
     context = {
         'sampleData' : sampleData,
-        'counter' : counter
+        'counter' : counter,
+        'sampleDataTitle' : sampleDataTitle
     }
 
     return render_template('uploaddocs.html', **context)
